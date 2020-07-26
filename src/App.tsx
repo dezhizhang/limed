1052a880df128cc108393a89370e4c7920b47875
zhangdezhi <zhangdezhi@inman.cc>
2020-07-26 21:59:47 +0800

    添加样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Button,{ ButtonType } from './components/Button/button'
-
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 function App() {
   return (
     <div className="App" style={{padding:10}}>
       <Button onClick={() => alert("hello")} btnType={ButtonType.Primary} className="custom">hello</Button>
+      <Menu>
+        <MenuItem>click1</MenuItem>
+        <MenuItem>click2</MenuItem>
+        <MenuItem>click3</MenuItem>
+      </Menu>
     </div>
   );
 }
