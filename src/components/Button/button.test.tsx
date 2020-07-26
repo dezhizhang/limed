1d54e3614b871a22e1504660fe6687b14f4b3dfa
zhangdezhi <zhangdezhi@inman.cc>
2020-07-26 16:38:13 +0800

    提交

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import{ render,fireEvent }  from '@testing-library/react';
-import Button,{ ButtonProps,} from './button';
+import Button,{ ButtonProps,ButtonSize,ButtonType } from './button';
 const defaultProps = {
     onClick:jest.fn()
+}
+
+const testProps:ButtonProps = {
+    btnType:ButtonType.Primary,
+    size:ButtonSize.Large,
+    className:'limed'
 }
 
 describe('test Button component',() => {
@@ -16,7 +22,10 @@ describe('test Button component',() => {
         expect(defaultProps.onClick).toHaveBeenCalled()
     })
     it('should render to corrent component based on different props',() => {
-
+        const weapper = render(<Button {...testProps}>hello</Button>);
+        const element = weapper.getByText('hello');
+        expect(element).toBeInTheDocument();
+        expect(element).toHaveClass('btn-primary btn-lg limed')
     })
     it('should render a link when btnType equals link and href is provided',() => {
 
