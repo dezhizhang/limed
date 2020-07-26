a2b8c4c15507d9d3e8926afbc95a2a7783b12ea0
zhangdezhi <zhangdezhi@inman.cc>
2020-07-26 16:33:05 +0800

    提交

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import{ render }  from '@testing-library/react';
-import Button from './button';
+import{ render,fireEvent }  from '@testing-library/react';
+import Button,{ ButtonProps,} from './button';
+const defaultProps = {
+    onClick:jest.fn()
+}
 
+describe('test Button component',() => {
+    it('should render the corrent default button',() => {
+        const weapper = render(<Button {...defaultProps}>hello</Button>);
+        const element = weapper.getByText('hello');
+        expect(element).toBeInTheDocument();
+        expect(element.tagName).toEqual('BUTTON');
+        expect(element).toHaveClass('btn btn-default');
+        fireEvent.click(element)
+        expect(defaultProps.onClick).toHaveBeenCalled()
+    })
+    it('should render to corrent component based on different props',() => {
 
-// test('test react button',() => {
-//     const weapper = render(<Button>button</Button>);
-//     const element = weapper.queryByText('button');
-//     expect(element).toBeTruthy();
-//     expect(element).toBeInTheDocument()
-// });
+    })
+    it('should render a link when btnType equals link and href is provided',() => {
 
-// describe('test Button component',() => {
-//     const weapper = 
-// })
+    })
+
+})
 
