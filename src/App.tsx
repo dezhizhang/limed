f009f76d425fed597c2c5eb60d22bc5a74682897
zhangdezhi <zhangdezhi@inman.cc>
2020-07-26 15:58:36 +0800

    添加添断言库

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button,{ButtonType,ButtonSize} from './components/Button/button'
+import Button,{ ButtonType } from './components/Button/button'
 
 function App() {
   return (
