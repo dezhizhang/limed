62fca762a57e6a31f5ea7cd625c83a76b99476bc
zhangdezhi <zhangdezhi@inman.cc>
2020-07-26 16:13:39 +0800

    提交

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -3,9 +3,14 @@ import{ render }  from '@testing-library/react';
 import Button from './button';
 
 
-test('test react button',() => {
-    const weapper = render(<Button>button</Button>);
-    const element = weapper.queryByText('button');
-    expect(element).toBeTruthy();
-});
+// test('test react button',() => {
+//     const weapper = render(<Button>button</Button>);
+//     const element = weapper.queryByText('button');
+//     expect(element).toBeTruthy();
+//     expect(element).toBeInTheDocument()
+// });
+
+// describe('test Button component',() => {
+//     const weapper = 
+// })
 
