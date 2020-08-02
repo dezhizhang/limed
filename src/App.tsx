82dede35b8ad78c09426ba37208bb5cf9e4dbeda
zhangdezhi <zhangdezhi@inman.cc>
2020-08-02 17:31:05 +0800

    添加图标库

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Button,{ ButtonType } from './components/Button/button'
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
+import Icon from './components/Icon/icon';
 function App() {
   return (
     <div className="App" style={{padding:10}}>
+      <Icon icon="coffee" theme="danger"/>
       <Button onClick={() => alert("hello")} btnType={ButtonType.Primary} className="custom">hello</Button>
       <Menu mode="vertical">
         <MenuItem>click1</MenuItem>
