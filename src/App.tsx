ea0e4a696fbae05818f9fc3120f7323174a2bc42
zhangdezhi <zhangdezhi@inman.cc>
2020-08-02 17:46:16 +0800

    icon组件

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Icon from './components/Icon/icon';
 function App() {
   return (
     <div className="App" style={{padding:10}}>
-      <Icon icon="coffee" theme="danger"/>
+      <Icon icon="coffee" theme="primary"/>
       <Button onClick={() => alert("hello")} btnType={ButtonType.Primary} className="custom">hello</Button>
       <Menu mode="vertical">
         <MenuItem>click1</MenuItem>
