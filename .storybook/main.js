e38780e72a62963ab205fa9a27d95b360c26524b
zhangdezhi <1541609448@qq.com>
2021-01-09 16:48:49 +0800

    添加文件

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,3 @@
 module.exports = {
-    typescript: {
-      check: false,
-      checkOptions: {},
-      reactDocgen: 'react-docgen-typescript',
-      reactDocgenTypescriptOptions: {
-        shouldExtractLiteralValuesFromEnum: true,
-        propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-      },
-    },
-  };
+    stories: ['../src/components/**/*.story.@(js|mdx)'],
+  }
