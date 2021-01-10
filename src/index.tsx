acedeced2c270cbff3f4632122b772fa9d69976c
zhangdezhi <1541609448@qq.com>
2021-01-10 16:47:05 +0800

    添加文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//button组件
+export { default as Button } from './components/Button';
+//input组件
+export { default as Input } from './components/Input';
+//menu组件
