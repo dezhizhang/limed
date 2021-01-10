0795cd260361e51a09c60c04a5eda65ec024eb47
zhangdezhi <1541609448@qq.com>
2021-01-10 16:54:36 +0800

    添加组件

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@ export { default as Button } from './components/Button';
 //input组件
 export { default as Input } from './components/Input';
 //menu组件
+export { default as Menu } from './components/Menu';
+
